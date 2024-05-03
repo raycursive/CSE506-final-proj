e25204f68aedb9a6530dbad993f8cbc5f2ea916f
sethu <sethu.kumar7@gmail.com>
2024-05-03 18:39:05 -0400

    Update script to add plots

--- process_result.py
+++ process_result.py
@@ -1,23 +1,51 @@
 import argparse
+import math
 import os
 from collections import defaultdict
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 # Index of values in the metrics result line
 METRIC_IND, MALLOC_IND, THREADS_IND, AVG_THROUGHPUT_IND = 0, 1, 2, -1
+
+
+def measurement_transform_single(x):
+    return round(x / 1e3)
+
+
+def measurement_transform(m):
+    m = list(map(float, m))
+    return list(map(measurement_transform_single, m))
+
+
+def plot_line(x_keys, data, x_label, y_label, title, y_lim_min, y_lim_max, output_file_name=None):
+    for attribute, vals in data.items():
+        plt.plot(x_keys, measurement_transform(vals), label=attribute, marker='x')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.legend(loc='best')  # , ncols=3)
+    plt.ylim(y_lim_min, y_lim_max)
+
+    # plt.show()
+    plt.savefig(output_file_name or 'image.png')
 
 
 def get_value(inp):
     return inp.split(':')[1].strip()
 
 
-def get_output_file_path(input_file_path, result_dir):
+def get_output_file_path(input_file_path, result_dir, extension='csv'):
     input_file_name = input_file_path.split('/')[-1]
-    res_file_path = os.path.join(result_dir, f"{input_file_name.split('.')[0]}_res.csv")
+    res_file_path = os.path.join(result_dir, f"{input_file_name.split('.')[0]}_res.{extension}")
 
     return res_file_path
 
 
-def compute_avg_per_thread_perf(input_file_path, result_dir):
+def compute_avg_per_thread_perf(input_file_path, data_structure, result_dir):
     thread_metric_map = defaultdict(lambda: defaultdict(list))
 
     with open(input_file_path) as handle:
@@ -41,6 +69,8 @@ def compute_avg_per_thread_perf(input_file_path, result_dir):
 
     mallocs = next(iter(thread_metric_map.values())).keys()
     res = [['threads', *mallocs]]
+    plot_data = defaultdict(list)
+    min_v, max_v = math.inf, -math.inf
 
     # for threads, mallocs_map in thread_metric_map.items():
     for threads in sorted(thread_metric_map.keys()):
@@ -51,6 +81,9 @@ def compute_avg_per_thread_perf(input_file_path, result_dir):
         for name in mallocs:
             avg = round(sum(mallocs_map[name]) / len(mallocs_map[name]))
             row.append(avg)
+            plot_data[name].append(avg)
+            min_v = min(min_v, avg)
+            max_v = max(avg, max_v)
 
         res.append(row)
 
@@ -62,12 +95,30 @@ def compute_avg_per_thread_perf(input_file_path, result_dir):
         for row in res:
             handle.write(','.join(map(str, row)) + '\n')
 
+    plot_input = {
+        'x_keys': ('1', '4', '8', '16', '32', '64'),
+        'data': plot_data,
+        'x_label': 'Number of threads',
+        'y_label': 'puts/sec in thousands',
+        'title': f'{data_structure} puts performance: Average throughput per thread',
+        'y_lim_min': min(0, measurement_transform_single(min_v) - 500),
+        'y_lim_max': measurement_transform_single(max_v) + 1000,
+        'output_file_name': get_output_file_path(input_file_path, result_dir, 'png')
+    }
+
+    plot_line(**plot_input)
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         dest="input_file_path",
+        action="store",
+    )
+
+    parser.add_argument(
+        dest="data_structure",
         action="store",
     )
 
@@ -85,4 +136,4 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     print("Processing result file:", args.input_file_path)
-    compute_avg_per_thread_perf(args.input_file_path, args.result_dir)
+    compute_avg_per_thread_perf(args.input_file_path, args.data_structure, args.result_dir)
--- test_th.sh
+++ test_th.sh
@@ -1,6 +1,7 @@
 ARGS=$1
 mkdir -p results
 RES_FILE_PATH=results/$2.txt
+DATA_STRUCTURE=$3
 
 for r in {1..3}; do
   echo Round $r;
@@ -11,4 +12,4 @@ for r in {1..3}; do
   echo;
   done
 
-python3 -m process_result $RES_FILE_PATH -rd processed
+python3 -m process_result $RES_FILE_PATH $DATA_STRUCTURE -rd processed
