64cb672e3d2ce1fc9959829adcc91ca85d23dc3c
sethu <sethu.kumar7@gmail.com>
2024-05-03 20:51:16 -0400

    Eval script changes

--- test_th.sh
+++ test_th.sh
@@ -1,15 +1,18 @@
+# Call format: ./test_th.sh "--tree <tree> --testcase <test_case>" <result_file_name> <data_structure_name>
+# data_structure_name - name to show in plots
+# result_file_name - name of the output file (without extension)
 ARGS=$1
 mkdir -p results
-RES_FILE_PATH=results/$2.txt
+RES_FILE_NAME=results/$2.txt
 DATA_STRUCTURE=$3
 
-for r in {1..3}; do
-  echo Round $r;
-  for i in 1 4 8 16 32 64; do
-        echo Executing for $i threads;
-        make test ARGS="$ARGS -p -j$i" >> "$RES_FILE_PATH";
-      done
-  echo;
-  done
+# for r in {1..3}; do
+#   echo Round $r;
+#   for i in 1 4 8 16 32 64; do
+#         echo Executing for $i threads;
+#         make test ARGS="$ARGS -p -j$i" >> "$RES_FILE_NAME";
+#       done
+#   echo;
+#   done
 
-python3 -m process_result $RES_FILE_PATH $DATA_STRUCTURE -rd processed
+python3 -m process_result "$RES_FILE_NAME" "$DATA_STRUCTURE" -rd processed
